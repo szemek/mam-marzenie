da197307c644fe6dbd5d696af7333a3b56b54505
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-02-08 23:17:39 +0100

    dream avatar

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -6,6 +6,8 @@ class Dream < ActiveRecord::Base
   validates :child_age, inclusion: { in: proc { |obj| obj.age_range } }
   validates :status, inclusion: { in: proc { statuses } }
 
+  mount_uploader :avatar, AvatarUploader
+
   def age_range
     3..18
   end
