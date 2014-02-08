e9bed5947fbe4b76f1e17af2b431e96655d3dd51
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-02-08 23:55:17 +0100

    simple_form applied on dream form

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -3,12 +3,13 @@ class Dream < ActiveRecord::Base
   has_many :members
   has_many :users, through: :members
 
-  validates :child_age, inclusion: { in: proc { |obj| obj.age_range } }
+  validates_presence_of :title, :description, :child_fullname, :region
+  validates :child_age, inclusion: { in: proc { age_range } }
   validates :status, inclusion: { in: proc { statuses } }
 
   mount_uploader :avatar, AvatarUploader
 
-  def age_range
+  def self.age_range
     3..18
   end
 
