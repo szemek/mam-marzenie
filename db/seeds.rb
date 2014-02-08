ca2f9ef0b0baff6a927163f2cb370935b13a41ec
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-02-08 11:42:34 +0100

    Alek will be happy with this commit ;-)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-User.create(:email => 'mam@marzenie.org', :password => 'marzenie', :password_confirmation => 'marzenie')
+User.create(email: 'mam@marzenie.org', password: 'marzenie', password_confirmation: 'marzenie')
