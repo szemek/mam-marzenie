8d15df247b28c52170d3b0f003eb9aa186ed1997
Grzegorz Lisowski <glisowski@fractalsoft.org>
2014-02-08 14:48:08 +0100

    search for voulonteers and sponsors added

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -3,4 +3,9 @@ class VolunteersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def search
+    @users = User.where('fullname LIKE ?', "%#{params[:fullname]}%")
+    render :index
+  end
 end
