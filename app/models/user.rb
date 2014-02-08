96fdaa2e8aef382a0dd91d3a538ef8d9bb8f0c99
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-02-08 15:11:43 +0100

    has_and_belongs_to_many association between user & dream

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_and_belongs_to_many :dreams
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
