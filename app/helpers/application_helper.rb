cbf1a90b244e3342b5db788c76a6fc183291bd7a
Agnieszka Matysek <amatysek@fractalsoft.org>
2014-02-08 19:20:49 +0100

    Locale to Volunteer show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,11 +6,11 @@ module ApplicationHelper
     names.each do |name|
       if current_page?(:controller => name)
         elements << content_tag(:li, class: 'active') do
-          link_to t(name), url_for(:controller => name)
+          link_to t(name, scope: :layouts), url_for(:controller => name)
         end
       else
         elements << content_tag(:li) do
-          link_to t(name), url_for(:controller => name)
+          link_to t(name, scope: :layouts), url_for(:controller => name)
         end
       end
     end
