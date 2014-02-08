31d195b2b2902c83b1aa453e7c5f53b9e9575d1d
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-02-08 12:27:23 +0100

    pl as a default locale

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module MamMarzenie
     config.i18n.load_path += Dir[
       Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
     ]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :pl
     config.i18n.available_locales = [:en, :pl]
     config.i18n.fallbacks = true
 
