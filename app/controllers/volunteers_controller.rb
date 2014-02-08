ae95a2e558f6e4a0039c14208e6e97c5f3871e80
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2014-02-08 15:34:40 +0100

    Volunteers#show

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -3,4 +3,8 @@ class VolunteersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- spec/controllers/volunteers_controller_spec.rb
+++ spec/controllers/volunteers_controller_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe VolunteersController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
+  describe 'GET index' do
+    it 'returns http success' do
       get 'index'
       expect(response).to be_success
     end
   end
 
+  describe 'GET show' do
+    it 'assigns the requested dream as @user' do
+      user = User.create! valid_attributes
+      get :show, { id: user.to_param }, valid_session
+      expect(assigns(:user)).to eq(user)
+    end
+  end
 end
