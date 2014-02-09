d47f5ab753463b806ac3aba130f21e538d8c6cd4
Grzegorz Lisowski <glisowski@fractalsoft.org>
2014-02-09 05:22:39 +0100

    added task editing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
 class TasksController < ApplicationController
-  def new
-    @task = Task.new
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to dream_path(@task.dream_id)
   end
 
   def create
     hash = task_params
-    hash[:sponsor] = Sponsor.where(fullname: hash[:sponsor]).first_or_create
     hash[:user] = current_user
     hash[:dream] = Dream.find(hash[:dream_id])
     @task = Task.create(hash.except(:dream_id))
@@ -14,9 +19,15 @@ class TasksController < ApplicationController
 
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(
-      :sponsor, :dream_id, :result, :description
+    hash = params.require(:task).permit(
+      :sponsor, :dream_id, :result, :description, :user_id
     )
+    hash[:sponsor] = Sponsor.where(fullname: hash[:sponsor]).first_or_create
+    hash.except(:dream_id, :user_id)
   end
 end
