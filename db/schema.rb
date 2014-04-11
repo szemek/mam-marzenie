72c3cc3db1890fd3a3bae990c09fdf42b1c3471a
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2014-04-11 07:56:25 +0200

    YAGNI, removed delayed_job gem and delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330213920) do
+ActiveRecord::Schema.define(version: 20140411055411) do
 
   create_table "comments", force: true do |t|
     t.integer  "dream_id"
@@ -20,22 +20,6 @@ ActiveRecord::Schema.define(version: 20140330213920) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "dreams", force: true do |t|
     t.string   "title"
